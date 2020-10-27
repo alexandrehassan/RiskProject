c6f62770abaf4a99df299898546dff079c490ae6
Jonah <jonahgaudet@rogers.com>
2020-10-27 16:19:34 -0400

    Improve the GUI (yes, it was worse before)

--- Risk/src/GameFrame.java
+++ Risk/src/GameFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
@@ -33,18 +34,24 @@ public class GameFrame extends JFrame implements GameView{
         abm.addGameView(this);
         GameController gameController = new GameController(abm);
 
-        JPanel panel1 = (JPanel) this.getContentPane();
+        JPanel mainPanel = (JPanel) this.getContentPane();
 
         this.updateLine = new JLabel("RISK: a multi-player game of world domination");
-        panel1.add(updateLine, BorderLayout.PAGE_START);
 
+        JPanel middlePane = new JPanel();
+        middlePane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        //Put the board in this panel (feel free to get rid of the image, just a placeholder)
+        JPanel board = new JPanel();
+        //board.add(createBoard());
         //Adds the image of the risk map
-        JLabel label1 = new JLabel();
+        JLabel placeholderBoard = new JLabel();
         ImageIcon image = new ImageIcon("Risk/images/riskmap.jpg");
         Image newImage = image.getImage().getScaledInstance(750, 500,  java.awt.Image.SCALE_SMOOTH);
-        label1.setIcon(new ImageIcon(newImage));
-        label1.setBounds(0, 0, 1100, 600);
-        panel1.add(label1, BorderLayout.LINE_START);
+        placeholderBoard.setIcon(new ImageIcon(newImage));
+        placeholderBoard.setBounds(0, 0, 1100, 600);
+        board.add(placeholderBoard);
 
         //Adds the boxes with the owned countries in them (right side)
         JPanel playerInfo = new JPanel();
@@ -52,20 +59,20 @@ public class GameFrame extends JFrame implements GameView{
         layout1.setVgap(5);
         layout1.setHgap(5);
         playerInfo.setLayout(layout1);
+        //playerInfo.setBorder(BorderFactory.createLineBorder(Color.black));
         for (int i = 0; i < 6; i++) {
             JTextArea textArea = new JTextArea(5, 20);
             textArea.setText("This is box " + Integer.toString(i));
-            textArea.setMaximumSize(new Dimension(100,100));
             JScrollPane scrollPane = new JScrollPane(textArea);
+            //scrollPane.setSize(new Dimension(200,200));
             playerInfo.add(scrollPane);
             textArea.setEditable(false);
             playersInfo.add(textArea);
         }
-        panel1.add(playerInfo, BorderLayout.LINE_END);
 
         //Add the command buttons
         JPanel buttonOptions = new JPanel();
-        GridLayout layout2 = new GridLayout(4,1);
+        GridLayout layout2 = new GridLayout(1,4);
         layout2.setVgap(25);
         layout2.setHgap(25);
         buttonOptions.setLayout(layout2);
@@ -74,23 +81,56 @@ public class GameFrame extends JFrame implements GameView{
             JButton b = new JButton(validCommands[i]);
             b.addActionListener(gameController);
             b.setActionCommand(b.getText());
-            b.setMaximumSize(new Dimension(100,75));
+            //b.setMaximumSize(new Dimension(75,75));
             buttonOptions.add(b);
         }
-        panel1.add(buttonOptions, BorderLayout.CENTER);
 
+        //Button to start the game
         JButton startButton = new JButton("Start Game");
         startButton.addActionListener(gameController);
         startButton.setActionCommand("new");
-        panel1.add(startButton, BorderLayout.PAGE_END);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(5,5,5,5);
+        middlePane.add(buttonOptions, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 2;
+        c.gridheight = 2;
+        c.gridx = 0;
+        c.gridy = 1;
+        middlePane.add(board, c);
+
+        c.fill = GridBagConstraints.VERTICAL;
+        c.gridwidth = 1;
+        c.gridheight = 3;
+        c.gridx = 2;
+        c.gridy = 0;
+        middlePane.add(playerInfo, c);
+
+        mainPanel.add(updateLine, BorderLayout.PAGE_START);
+        //mainPanel.add(label1, BorderLayout.LINE_START);
+        mainPanel.add(middlePane, BorderLayout.CENTER);
+        //mainPanel.add(playerInfo, BorderLayout.LINE_END);
+        mainPanel.add(startButton, BorderLayout.PAGE_END);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("JList Example");
-        this.setSize(1400,575);
+        this.setSize(1300,650);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
+    /**
+     * Should return the board, created in this method:
+     */
+    private void createBoard () {
+        //get the board
+    }
     public void handleGameUpdate(GameEvent gameModel) {
 
     }
