82d1737604fd9b954fb350a6e3bf5b289278e7e1
alex <alexhassan@live.ca>
2020-10-30 16:45:03 -0400

    Modified Continent.addCountries to prevent duplicates.

--- Risk/tests/ContinentTest.java
+++ Risk/tests/ContinentTest.java
@@ -1,28 +1,73 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ContinentTest {
+    private static String NAME = "TestName";
+    private static int TROOPS = 3;
+    private static Continent CONTINENT;
+    private static ArrayList<Country> countries;
+    private static Country country1, country2, country3, country4;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+        CONTINENT = new Continent(NAME, TROOPS);
+
+        countries = new ArrayList<>();
+        countries.add(country1 = new Country("country1"));
+        countries.add(country2 = new Country("country2"));
+        countries.add(country3 = new Country("country3"));
+        countries.add(country4 =new Country("country4"));
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getName() {
+        Continent continent = new Continent(NAME, TROOPS);
+        assertTrue(continent.getName().equals(NAME));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void continentNameNull(){
+        Continent continent = new Continent("null", TROOPS);
+        assertTrue(continent.getName() != null);
+    }
+
+    @Test
+    void twoContinentSameNameNotEqual(){
+        Continent continent1 = new Continent(NAME, TROOPS);
+        Continent continent2 = new Continent(NAME, TROOPS);
+        assertTrue (continent1 != continent2);
+    }
+
+    @Test
     void getCountries() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getReinforcements() {
     }
 
-    @org.junit.jupiter.api.Test
-    void addCountries() {
+    @Test
+    void addCountries(){
+        CONTINENT.addCountries(countries);
+        ArrayList<Country> expected = (ArrayList<Country>) CONTINENT.getCountries().clone();
+
+        CONTINENT.addCountries(countries);
+
+        assert(expected.equals(CONTINENT.getCountries()));
+
+        for(Country country: CONTINENT.getCountries()){
+            System.out.println(country);
+        }
+
     }
 }
