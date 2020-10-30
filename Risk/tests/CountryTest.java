a9d82feb2e5da8c5f9a024299677770912f99a0b
Alexandre Hassan <alexhassan@live.ca>
2020-10-30 18:03:40 -0400

    Created CountryTest and wrote basic tests.

--- Risk/tests/CountryTest.java
+++ Risk/tests/CountryTest.java
@@ -1,42 +1,92 @@
-class CountryTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    @org.junit.jupiter.api.BeforeEach
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Series of Tests for class Country.
+ * @author Alexandre Hassan
+ * @Version 2020-10-30
+ */
+class CountryTest {
+    private static final String NAME = "TestName";
+    private Country COUNTRY;
+    private static ArrayList<Country> neighbors;
+    private static final int InitialTroops = 1;
+
+    @BeforeEach
     void setUp() {
+        COUNTRY = new Country(NAME);
+
+        neighbors = new ArrayList<>();
+        neighbors.add(new Country("country1"));
+        neighbors.add(new Country("country2"));
+        neighbors.add(new Country("country3"));
+        neighbors.add(new Country("country4"));
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getName() {
+        assertEquals(NAME, COUNTRY.getName());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void countryNameNull(){
+        Country country = new Country("null");
+        assertNotNull(country.getName());
+    }
+
+    @Test
     void addNeighbor() {
+        for (Country neighbor : neighbors) {
+            COUNTRY.addNeighbor(neighbor);
+            assertTrue(COUNTRY.hasNeighbor(neighbor));
+            assertTrue(COUNTRY.getNeighbors().contains(neighbor));
+        }
     }
 
-    @org.junit.jupiter.api.Test
-    void hasNeighbor() {
+    @Test
+    void addTroopNegative() {
+        COUNTRY.addTroop(-1);
+        assertEquals(COUNTRY.getTroops(),InitialTroops);
     }
 
-    @org.junit.jupiter.api.Test
-    void getNeighbors() {
+    @Test
+    void addTroopZero() {
+        COUNTRY.addTroop(0);
+        assertEquals(COUNTRY.getTroops(),InitialTroops);
     }
 
-    @org.junit.jupiter.api.Test
-    void addTroop() {
+    @Test
+    void addTroopPositive() {
+        COUNTRY.addTroop(1);
+        assertEquals(COUNTRY.getTroops(),InitialTroops + 1);
     }
 
-    @org.junit.jupiter.api.Test
-    void getTroops() {
+
+    @Test
+    void removeTroopNegative() {
+        COUNTRY.removeTroops(-1);
+        assertEquals(COUNTRY.getTroops(),InitialTroops);
     }
 
-    @org.junit.jupiter.api.Test
-    void removeTroops() {
+    @Test
+    void removeTroopZero() {
+        COUNTRY.removeTroops(0);
+        assertEquals(COUNTRY.getTroops(),InitialTroops);
     }
 
-    @org.junit.jupiter.api.Test
-    void testToString() {
+    @Test
+    void removeTroopPositive() {
+        COUNTRY.removeTroops(1);
+        assertEquals(COUNTRY.getTroops(),InitialTroops - 1);
     }
 }
