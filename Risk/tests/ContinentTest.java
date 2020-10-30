afb81836b6abd2466fb929072eb50e705f26773f
Alexandre Hassan <alexhassan@live.ca>
2020-10-30 17:54:10 -0400

    AddedDocString

--- Risk/tests/ContinentTest.java
+++ Risk/tests/ContinentTest.java
@@ -4,25 +4,28 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Series of Tests for class Continent.
+ * @author Alexandre Hassan
+ * @version 2020-10-30
+ */
 class ContinentTest {
-    private static String NAME = "TestName";
-    private static int TROOPS = 3;
+    private static final String NAME = "TestName";
+    private static final int TROOPS = 3;
     private static Continent CONTINENT;
     private static ArrayList<Country> countries;
-    private static Country country1, country2, country3, country4;
 
     @BeforeEach
     void setUp() {
         CONTINENT = new Continent(NAME, TROOPS);
 
         countries = new ArrayList<>();
-        countries.add(country1 = new Country("country1"));
-        countries.add(country2 = new Country("country2"));
-        countries.add(country3 = new Country("country3"));
-        countries.add(country4 =new Country("country4"));
+        countries.add(new Country("country1"));
+        countries.add(new Country("country2"));
+        countries.add(new Country("country3"));
+        countries.add(new Country("country4"));
     }
 
     @AfterEach
@@ -32,25 +35,25 @@ class ContinentTest {
     @Test
     void getName() {
         Continent continent = new Continent(NAME, TROOPS);
-        assertTrue(continent.getName().equals(NAME));
+        assertEquals(NAME, continent.getName());
     }
 
     @Test
     void continentNameNull(){
         Continent continent = new Continent("null", TROOPS);
-        assertTrue(continent.getName() != null);
+        assertNotNull(continent.getName());
     }
 
     @Test
     void twoContinentSameNameNotEqual(){
         Continent continent1 = new Continent(NAME, TROOPS);
         Continent continent2 = new Continent(NAME, TROOPS);
-        assertTrue (continent1 != continent2);
+        assertNotSame(continent1, continent2);
     }
 
     @Test
     void getCountriesEmpty() {
-        assertTrue(CONTINENT.getCountries().equals(new ArrayList<>()));
+        assertEquals(new ArrayList<>(), CONTINENT.getCountries());
     }
 
     @Test
